427537de750d8f24da7d4cb3e7532faab7be4ae4
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-09-28 19:50:40 -0300

    chore: add bcrypt_elixir

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule PhoenixPay.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:bcrypt_elixir, "~> 2.0"}
     ]
   end
 
