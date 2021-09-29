0b85fae2a9bb9d2e923ec7886084d7d3a8b6a7f7
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-09-28 21:23:46 -0300

    chore: create users view

---
+++ lib/phoenix_pay_web/views/users_view.ex
@@ -0,0 +1,14 @@
+defmodule PhoenixPayWeb.UsersView do
+  alias PhoenixPay.User
+
+  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+    %{
+      message: "User created",
+      user: %{
+        id: id,
+        name: name,
+        nickname: nickname
+      }
+    }
+  end
+end
