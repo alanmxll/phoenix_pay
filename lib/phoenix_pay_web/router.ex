2f44ab74bd7194a138b36903046ebe6f2afc9e89
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-09-30 18:59:10 -0300

    chore: create routes do deposit and withdraw to accounts controller

--- lib/phoenix_pay_web/router.ex
+++ lib/phoenix_pay_web/router.ex
@@ -11,6 +11,9 @@ defmodule PhoenixPayWeb.Router do
     get "/:filename", WelcomeController, :index
 
     post "/users", UsersController, :create
+
+    post "/accounts/:id/deposit", AccountsController, :deposit
+    post "/accounts/:id/withdraw", AccountsController, :withdraw
   end
 
   # Enables LiveDashboard only for development
