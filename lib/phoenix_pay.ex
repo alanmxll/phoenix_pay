0b089e081c40c72b130766fe71d5a00af5f316a7
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-09-30 19:35:52 -0300

    chore: add defdelegate to deposit as facade

--- lib/phoenix_pay.ex
+++ lib/phoenix_pay.ex
@@ -1,5 +1,7 @@
 defmodule PhoenixPay do
   alias PhoenixPay.Users.Create, as: UserCreate
+  alias PhoenixPay.Accounts.Deposit
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate deposit(params), to: Deposit, as: :call
 end
