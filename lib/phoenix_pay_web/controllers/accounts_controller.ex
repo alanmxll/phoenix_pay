931642548ce968c4c9ff5d15eb74d6df6e10a696
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-09-30 19:36:39 -0300

    chore: create accounts controller with deposit action

---
+++ lib/phoenix_pay_web/controllers/accounts_controller.ex
@@ -0,0 +1,15 @@
+defmodule PhoenixPayWeb.AccountsController do
+  use PhoenixPayWeb, :controller
+
+  alias PhoenixPay.Account
+
+  action_fallback PhoenixPayWeb.FallbackController
+
+  def deposit(conn, params) do
+    with {:ok, %Account{} = account} <- PhoenixPay.deposit(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+    end
+  end
+end
