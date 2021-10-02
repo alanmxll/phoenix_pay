d5cee2d09d6687955742669e91d1986eac2aadaf
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-10-02 18:50:35 -0300

    chore: create transaction struct

---
+++ lib/phoenix_pay/accounts/transactions/response.ex
@@ -0,0 +1,12 @@
+defmodule PhoenixPay.Accounts.Transactions.Response do
+  alias PhoenixPay.Account
+
+  defstruct [:from_account, :to_account]
+
+  def build(%Account{} = from_account, %Account{} = to_account) do
+    %__MODULE__{
+      from_account: from_account,
+      to_account: to_account
+    }
+  end
+end
