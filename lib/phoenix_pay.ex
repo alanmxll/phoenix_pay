1d965d8fdca00a781a671f669ac3d1282b595a75
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-10-02 13:30:29 -0300

    chore: implements action withdraw

--- lib/phoenix_pay.ex
+++ lib/phoenix_pay.ex
@@ -1,7 +1,8 @@
 defmodule PhoenixPay do
   alias PhoenixPay.Users.Create, as: UserCreate
-  alias PhoenixPay.Accounts.Deposit
+  alias PhoenixPay.Accounts.{Deposit, Withdraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate deposit(params), to: Deposit, as: :call
+  defdelegate withdraw(params), to: Withdraw, as: :call
 end
--- lib/phoenix_pay_web/controllers/accounts_controller.ex
+++ lib/phoenix_pay_web/controllers/accounts_controller.ex
@@ -12,4 +12,12 @@ defmodule PhoenixPayWeb.AccountsController do
       |> render("update.json", account: account)
     end
   end
+
+  def withdraw(conn, params) do
+    with {:ok, %Account{} = account} <- PhoenixPay.withdraw(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+    end
+  end
 end
