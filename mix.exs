090a9c1f57903bc13dbc8b7e066e3b5a4ef984e3
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-10-03 10:27:21 -0300

    chore: configure excoveralls

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule PhoenixPay.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -46,7 +53,8 @@ defmodule PhoenixPay.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:bcrypt_elixir, "~> 2.0"},
-      {:decimal, "~> 2.0"}
+      {:decimal, "~> 2.0"},
+      {:excoveralls, "~> 0.14.2", only: :test}
     ]
   end
 
