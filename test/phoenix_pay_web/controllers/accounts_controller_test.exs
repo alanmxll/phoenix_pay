b29106a7d91bb73f6b15eb3e2bb430b0d8c7c2d4
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-10-03 12:15:26 -0300

    test: implements account controller deposit test

---
+++ test/phoenix_pay_web/controllers/accounts_controller_test.exs
@@ -0,0 +1,50 @@
+defmodule PhoenixPayWeb.AccountsControllerTest do
+  use PhoenixPayWeb.ConnCase
+
+  alias PhoenixPay.{Account, User}
+
+  describe "deposit/2" do
+    setup %{conn: conn} do
+      params = %{
+        name: "Alan",
+        password: "123456",
+        nickname: "alanmxll",
+        email: "alanmxll@mail.com",
+        age: 28
+      }
+
+      {:ok, %User{account: %Account{id: account_id}}} = PhoenixPay.create_user(params)
+
+      conn = put_req_header(conn, "authorization", "Basic YWxhbm14bGw6cGhvZW5peDEyMw==")
+
+      {:ok, conn: conn, account_id: account_id}
+    end
+
+    test "when all params are valid, make the deposit", %{conn: conn, account_id: account_id} do
+      params = %{"value" => "50.0"}
+
+      response =
+        conn
+        |> post(Routes.accounts_path(conn, :deposit, account_id, params))
+        |> json_response(:ok)
+
+      assert %{
+               "account" => %{"balance" => "50.00", "id" => _id},
+               "message" => "Ballance changed successfully"
+             } = response
+    end
+
+    test "when there are invalid params, returns an error", %{conn: conn, account_id: account_id} do
+      params = %{"value" => "test"}
+
+      response =
+        conn
+        |> post(Routes.accounts_path(conn, :deposit, account_id, params))
+        |> json_response(:bad_request)
+
+      expected_response = %{"message" => "Invalid deposit value!"}
+
+      assert response == expected_response
+    end
+  end
+end
