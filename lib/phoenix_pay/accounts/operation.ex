3916e9c9a610ad261144d3fc5ced321c6caeb620
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-10-04 20:16:39 -0300

    refactor: add operation name on error message invalid value

--- lib/phoenix_pay/accounts/operation.ex
+++ lib/phoenix_pay/accounts/operation.ex
@@ -36,7 +36,7 @@ defmodule PhoenixPay.Accounts.Operation do
 
   defp handle_cast({:ok, value}, balance, :deposit), do: Decimal.add(balance, value)
   defp handle_cast({:ok, value}, balance, :withdraw), do: Decimal.sub(balance, value)
-  defp handle_cast(:error, _balance, _operation), do: {:error, "Invalid deposit value!"}
+  defp handle_cast(:error, _balance, operation), do: {:error, "Invalid #{operation} value!"}
 
   defp update_account({:error, _reason} = error, _repo, _account), do: error
 
